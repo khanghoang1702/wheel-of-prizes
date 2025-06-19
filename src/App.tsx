468cdcd89f5fbe57949fbbcac01dd218bb81560a
khanghoangit <khanghoanghm@zigvy.com>
2025-06-19 18:04:41 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div style={{width: '100%'}}>
+      <div>
+        <img src="./assets/logo.jpeg" alt="logo" width={50}  />
+      </div>
+    </div>
+  )
+}
+
+export default App
